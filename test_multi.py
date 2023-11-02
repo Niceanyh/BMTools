14610d453f6858814933164b82117f0920b4d117
Niceanyh <niceanyh@gmail.com>
2023-11-02 20:52:44 +0800

    multi-tool tester for CPM-Bee

--- test_multi.py
+++ test_multi.py
@@ -1,12 +1,17 @@
 from bmtools.agent.tools_controller import load_valid_tools, MTQuestionAnswerer
+from bmtools.models.cpmbee_model import CpmBeeLLM
 tools_mappings = {
     'weather': "http://127.0.0.1:8079/tools/weather/",
     'file_operation': "http://127.0.0.1:8079/tools/file_operation/",
 }
 
 tools = load_valid_tools(tools_mappings)
+# SET config_path and ckpt_path
+config_path = ""
+ckpt_path = ""
+llm =CpmBeeLLM(config_path = config_path,  ckpt_path = ckpt_path, device="cuda")
 
-qa =  MTQuestionAnswerer(openai_api_key='', all_tools=tools)
+qa =  MTQuestionAnswerer(llm, all_tools=tools)
 
 agent = qa.build_runner()
 
